c51dc8811cb65aa1ae17eba675ef5bfe35334176
TranquilMarmot <nate272@gmail.com>
2019-06-01 13:52:02 -0700

    Clean up Hand

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ const App: FunctionComponent = () => {
 
   return (
     <div className="App">
+      {/* Haven't select the number of cards yet, show buttons to do so */}
       {numberOfCards < 0 && (
         <div>
+          <div>How many cards?</div>
           <button onClick={() => setNumberOfCards(4)}>4</button>
           <button onClick={() => setNumberOfCards(5)}>5</button>
         </div>
       )}
+
+      {/* Have selected the number of cards, show the hand */}
       {numberOfCards > 0 && <Hand numberOfCards={numberOfCards} />}
     </div>
   );
--- src/Hand.tsx
+++ src/Hand.tsx
@@ -10,15 +10,26 @@ import Card from "./Card";
 import { ActionTypes } from "./actions";
 import { Card as CardType } from "./types";
 
+// this initial state and context are created on first render
 let initialState: CardType[];
-
 export let CardsContext: Context<[CardType[], ((action: ActionTypes) => void)]>;
+
+const renderCards = (numberOfCards: number) => {
+  const cards: JSX.Element[] = [];
+
+  for (let i = 0; i < numberOfCards; i++) {
+    cards.push(<Card index={i} key={`card-${i}`} />);
+  }
+
+  return cards;
+};
 
 interface HandProps {
   numberOfCards: number;
 }
 
 const Hand: FunctionComponent<HandProps> = ({ numberOfCards }) => {
+  // create our initial state and context if we haven't yet
   if (!initialState || !CardsContext) {
     initialState = getInitialState(numberOfCards);
     CardsContext = createContext<[CardType[], ((action: ActionTypes) => void)]>(
@@ -26,16 +37,10 @@ const Hand: FunctionComponent<HandProps> = ({ numberOfCards }) => {
     );
   }
 
-  const cards: JSX.Element[] = [];
-
-  for (let i = 0; i < numberOfCards; i++) {
-    cards.push(<Card index={i} key={`card-${i}`} />);
-  }
-
   return (
     <div className="App">
       <CardsContext.Provider value={useReducer(CardsReducer, initialState)}>
-        {cards}
+        {renderCards(numberOfCards)}
       </CardsContext.Provider>
     </div>
   );
