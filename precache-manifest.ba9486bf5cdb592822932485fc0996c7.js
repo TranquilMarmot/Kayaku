caa461682a949d3fd651d5e4ea862a7f1990b053
TranquilMarmot <nate272@gmail.com>
2019-06-08 10:31:10 -0700

    Updates

---
+++ precache-manifest.ba9486bf5cdb592822932485fc0996c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e974091e9c2b026a87752e197bffd561",
+    "url": "/kayaku/index.html"
+  },
+  {
+    "revision": "e15dda7b765c38ad07bb",
+    "url": "/kayaku/static/css/main.b2af7f72.chunk.css"
+  },
+  {
+    "revision": "bcd66ca3c8faec4efcf4",
+    "url": "/kayaku/static/js/2.f28d18c5.chunk.js"
+  },
+  {
+    "revision": "e15dda7b765c38ad07bb",
+    "url": "/kayaku/static/js/main.27714c53.chunk.js"
+  },
+  {
+    "revision": "1d24817649930725d9ec",
+    "url": "/kayaku/static/js/runtime~main.fbe40461.js"
+  }
+]);
